1aebbf34439df24facd10940ca4a9ef400700c64
Ivan Luis <ivanthebest10@gmail.com>
2021-05-07 18:36:24 -0500

    Delete firebase.js

--- src/firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyA2O_vIOz90-_kd8mORbHm9QAgKPR0vrqE",
-    authDomain: "gastos-91020.firebaseapp.com",
-    databaseURL: "https://gastos-91020-default-rtdb.firebaseio.com",
-    projectId: "gastos-91020",
-    storageBucket: "gastos-91020.appspot.com",
-    messagingSenderId: "499424437489",
-    appId: "1:499424437489:web:2ce4852519c9afc3c87550"
-  };
