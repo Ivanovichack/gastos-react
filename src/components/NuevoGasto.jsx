2e4448c2cb8021da25d48764ee3b302f4c72a33f
IVAN LUIS JIMENEZ <iluisj@ad.elektra.com.mx>
2021-05-18 10:07:54 -0500

    combinando

--- src/components/NuevoGasto.jsx
+++ src/components/NuevoGasto.jsx
@@ -1,14 +1,36 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { FirebaseDatabaseMutation } from "@react-firebase/database";
+import firebase from "firebase/app";
+import "firebase/database";
+import {
+  FirebaseDatabaseProvider
+} from "@react-firebase/database";
+import { firebaseConfig } from "../firebase";
 
 class NuevoGasto extends Component {    
-
+//https://react-firebase-js.com/docs/guides/build-a-react-app-with-firebase-auth-and-realtime-database/write-data
     render() {
         return (
-            <div>
-             
-            </div>
+           <>
+           <FirebaseDatabaseProvider firebase={firebase} {...firebaseConfig}>
+            <FirebaseDatabaseMutation type="push" path="gastos">
+            {({ runMutation }) => (                
+                <button
+                    onClick={ async ev => {
+                        await runMutation({
+                            link_url: 0,
+                            link_description: 1,
+                            created_at: 2,
+                            updated_at: 3
+                          });
+                    }}                    
+                >                                    
+                </button>
+            )}
+            </FirebaseDatabaseMutation>
+            </FirebaseDatabaseProvider>
+           </>
         );
     }
 }
